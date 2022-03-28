6bc5c4a5ab82bd5ed1d9649abb85a4c9a2a72da9
Denis Lushnevskiy <fantazer171@mail.ru>
2022-03-29 03:10:19 +0700

    need fix error:ArrayIndexOutOfBoundsException

--- src/tables/table1/PokerHand.java
+++ src/tables/table1/PokerHand.java
@@ -1,34 +1,40 @@
 package tables.table1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PokerHand implements Comparable {
-    private String[] cards;
-    private int[] valuesOfCards = new int[5];
+    private String cards;
+    private int[] strengthHand = new int[4];
+
 
     public static void main(String[] args) {
         List<PokerHand> hands = new ArrayList<PokerHand>();
-        PokerHand hand1 = new PokerHand("9S 2H QC JD AD");
+        PokerHand hand1 = new PokerHand("QS 9H QC QD 9D");
 
-        for (int k = 0; k < hand1.valuesOfCards.length; k++ ) {
-            System.out.println(hand1.valuesOfCards[k]);
+        for (int k = 0; k < hand1.strengthHand.length; k++) {
+            System.out.println(hand1.strengthHand[k]);
         }
 
 
     }
 
-    public PokerHand(String cards) {
-        this.ConversionSymbolToValue(cards);
+    public PokerHand(String allCards) {
+        this.cards = allCards;
+        this.convertSymbolToValue(cards);
     }
 
-    private void ConversionSymbolToValue(String text) {                               /** нужно добавить обработку ошибки ArrayOutOfBounds*/
+    private void convertSymbolToValue(String text) {                        /** нужно добавить обработку ошибки ArrayOutOfBounds*/
+        int[] values = new int[5];
         for (int i = 0; i < text.length(); i += 3) {
-            valuesOfCards[i/3] = ConversionStringToNumeric(text.charAt(i));
+            values[i / 3] = convertStringToNumeric(text.charAt(i));
         }
+        Arrays.sort(values);
+        strengthHand = convertCardValuesToStrengthOfHand(values);
     }
 
-    private static int ConversionStringToNumeric(char ch) {
+    private int convertStringToNumeric(char ch) {
         int Value = -1;
         if (Character.isDigit(ch)) {
             Value = Character.getNumericValue(ch);
@@ -37,7 +43,7 @@ public class PokerHand implements Comparable {
                 case 'T':
                     Value = 10;
                     break;
-                case 'J' :
+                case 'J':
                     Value = 11;
                     break;
                 case 'Q':
@@ -52,6 +58,69 @@ public class PokerHand implements Comparable {
             }
         }
         return Value;
+    }
+
+    private int[] convertCardValuesToStrengthOfHand(int[] cardsValues) {
+        int combination = 0;
+        int highInCombination = 0;
+        int[] strength = new int[4];
+        for (int i = 0; i < cardsValues.length; i++) {
+            if (cardsValues[i] == cardsValues[i + 1]) {                             /** check Combination */
+                combination = +2;
+                highInCombination = checkHighInCombination(cardsValues, i);
+            }
+            for (int j = i + 1; j < cardsValues.length; j++) {
+                if (cardsValues[j] == cardsValues[j + 1]) {
+                    combination = +2;
+                    highInCombination = checkHighInCombination(cardsValues, j);
+                }
+            }
+        }
+
+        if (combination == 0) {
+            combination = checkStraight(cardsValues);
+        }
+        strength[0] = combination;
+        strength[1] = highInCombination;
+        strength[2] = cardsValues[4];
+        strength[3] = checkSecondHigh(cardsValues);
+
+        return strength;
+    }
+
+    private int checkHighInCombination(int[] cardsValues, int k) {
+        int highInCombination = 0;
+        if (cardsValues[k] == highInCombination) {
+            highInCombination = cardsValues[k] * 10;
+        } else if (cardsValues[k] > highInCombination) {
+            highInCombination = cardsValues[k];
+        }
+        return highInCombination;
+    }
+
+    private int checkStraight(int[] cardsValues) {
+        boolean straight = true;
+        for (int i = 0; i < cardsValues.length - 1; i++) {
+            if (cardsValues[i + 1] - cardsValues[i] != 1) {
+                straight = false;
+            }
+        }
+        if (straight) {
+            return 5;
+        } else {
+            return 0;
+        }
+    }
+
+    private int checkSecondHigh(int[] cardsValues) {
+        int secondHigh = 0;
+        for (int i = cardsValues.length - 1; i >= 0; i--) {
+            if (cardsValues[i] != cardsValues[i - 1]) {
+                secondHigh = cardsValues[i - 1];
+                break;
+            }
+        }
+        return secondHigh;
     }
 
     @Override
